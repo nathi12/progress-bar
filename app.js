d86a804eba330b068990f9e88d22e82f0a1ebe99
nathi12 <nathisamukelo90@gmail.com>
2024-01-10 21:21:52 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const btnTest = document.querySelector('#test');
-const btnTest1 = document.querySelector('#test1');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
 const circle1 = document.querySelector('.circle-1');
 const circle2 = document.querySelector('.circle-2');
 
@@ -16,7 +16,7 @@ const line2 = document.querySelector('.line-2');
 const line3 = document.querySelector('.line-3');
 
 
-btnTest.addEventListener('click', () => {
+btnLeft.addEventListener('click', () => {
 
     if (circle1.contains(longCircleText1)) {
         circle1.removeChild(longCircleText1);
@@ -26,24 +26,29 @@ btnTest.addEventListener('click', () => {
     circle1.classList.toggle('long-circle-1');
     circle1.classList.toggle('circle-1');
 
+    //moving to normal circle-2 when circle-1 in long circle
+    if (circle2.classList.contains('circle-2-moved')) {
+        circle2.classList.add('circle-2');
+    }
+
     //moving stuff
     line2.classList.toggle('line-2-moved');
     circle2.classList.toggle('circle-2-moved');
     line3.classList.toggle('line-3-moved');
 
 
+
     if (circle2.classList.contains('long-circle-2')) {
         circle2.classList.remove('long-circle-2');
         circle2.classList.add('circle-2-moved');
         circle2.removeChild(longCircleText2);
-
         line3.classList.remove('line-3-moved-1');
         line3.classList.add('line-3-moved');
     }
 });
 
 
-btnTest1.addEventListener('click', () => {
+btnRight.addEventListener('click', () => {
 
     if (circle2.contains(longCircleText2)) {
         circle2.removeChild(longCircleText2);
@@ -51,9 +56,29 @@ btnTest1.addEventListener('click', () => {
         circle2.appendChild(longCircleText2);
     }
 
+    if (circle2.classList.contains('circle-2-moved')) {
+        circle2.classList.remove('circle-2-moved');
+        circle2.classList.add('circle-2');
+        line3.classList.add('line-3');
+    }
+
     circle2.classList.toggle('long-circle-2');
     circle2.classList.toggle('circle-2');
-    line3.classList.toggle('line-3-moved-1');
+
+
+    //trigger for line 3
+    if (circle2.classList.contains('long-circle-2')) {
+        line3.classList.add('line-3-moved-1');
+    } else {
+        line3.classList.remove('line-3-moved-1');
+        line3.classList.remove('line-3-moved');
+        line3.classList.add('line-3');
+    }
+
+    if (line3.classList.length === 0) {
+        line3.classList.add('line-3');
+    }
+
 
 
     if (circle1.classList.contains('long-circle-1')) {
